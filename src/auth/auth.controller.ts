96652fbc57b87801532d6d0269b05238850067e1
Nottoodaay <odikadzegiorgi4@gmail.com>
2025-02-09 12:08:14 -0800

    feat: add user registration and login logics

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() body: { username: string; password: string }) {
+    const user = await this.authService.validateUser(
+      body.username,
+      body.password,
+    );
+    if (!user) {
+      return { message: 'Invalid username or password' };
+    }
+    return this.authService.login(user);
+  }
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  async register(@Body() body: { userName: string; password: string }) {
+    return this.usersService.create(body.userName, body.password);
+  }
+}
